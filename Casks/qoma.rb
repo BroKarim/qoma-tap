3e80bab42fc54bdfdfa2e8fbbd752160b18a949b
BroKarim <bukutulis0110@gmail.com>
2026-06-11 15:23:40 +0700

    Update download URL to BroKarim/qoma

--- Casks/qoma.rb
+++ Casks/qoma.rb
@@ -1,7 +1,7 @@
 cask "qoma" do
   version "1.0.0"
   sha256 "edf527470aac94c63cfcbb74a245ba2068ee619e697ee71e03b835f2b02e3ff7"
-  url "https://github.com/dzenn-app/pomodoro/releases/download/v#{version}/qoma-#{version}-mac-universal.dmg"
+  url "https://github.com/BroKarim/qoma/releases/download/v#{version}/qoma-#{version}-mac-universal.dmg"
 
   name "Qoma"
   desc "Minimal pomodoro timer"
