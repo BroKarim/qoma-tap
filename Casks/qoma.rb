0feae4d250b8fc165560bc4359e6de4db1fb8ff1
BroKarim <bukutulis0110@gmail.com>
2026-06-11 15:01:33 +0700

    Release v1.0.0: switch to universal DMG

--- Casks/qoma.rb
+++ Casks/qoma.rb
@@ -1,15 +1,8 @@
 cask "qoma" do
   version "1.0.0"
+  sha256 "edf527470aac94c63cfcbb74a245ba2068ee619e697ee71e03b835f2b02e3ff7"
+  url "https://github.com/dzenn-app/pomodoro/releases/download/v#{version}/qoma-#{version}-mac-universal.dmg"
 
-  on_arm do
-    sha256 "ec2e3752bc7dbcfae280dc830cbf5da832fdfd51a10fb7253eb7f42a1c1da497"
-    url "https://github.com/dzenn-app/pomodoro/releases/download/v#{version}/qoma-#{version}-mac-arm64.dmg"
-  end
-
-  on_intel do
-    sha256 "a18c48a737aae8f5a00869f3a9a45ea8d696d097752d83d73b4dfe543556bb55"
-    url "https://github.com/dzenn-app/pomodoro/releases/download/v#{version}/qoma-#{version}-mac-intel.dmg"
-  end
   name "Qoma"
   desc "Minimal pomodoro timer"
   homepage "https://dzenn.app"
