9790bc89042d22d207fd1d42567e25d34be01a1b
BroKarim <bukutulis0110@gmail.com>
2026-06-11 17:42:31 +0700

    fix(qoma): update download URL and app name

--- Casks/qoma.rb
+++ Casks/qoma.rb
@@ -1,16 +1,16 @@
 cask "qoma" do
   version "1.0.0"
   sha256 "edf527470aac94c63cfcbb74a245ba2068ee619e697ee71e03b835f2b02e3ff7"
-  url "https://github.com/BroKarim/qoma/releases/download/v#{version}/qoma_#{version}_mac-universal.dmg"
 
+  url "https://github.com/BroKarim/qoma/releases/download/v#{version}/Qoma-#{version}-universal.dmg"
   name "Qoma"
   desc "Minimal pomodoro timer"
   homepage "https://dzenn.app"
 
-  app "qoma.app"
+  app "Qoma.app"
 
   caveats <<~EOS
     If macOS blocks the app (unsigned), go to:
-    System Settings -> Privacy & Security -> Open Anyway.
+    System Settings → Privacy & Security → Open Anyway.
   EOS
 end
